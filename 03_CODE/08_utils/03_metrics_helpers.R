c6a592f9ced747495223fcda7b7d2bbc0b6266fa
fnroseror <143451856+fnroseror@users.noreply.github.com>
2026-04-02 11:32:29 -0500

    refactor: close 08_utils with transversal repository utilities

--- 03_CODE/08_utils/03_metrics_helpers.R
+++ 03_CODE/08_utils/03_metrics_helpers.R
@@ -1,1 +1,21 @@
+rmse_fun <- function(obs, pred) {
+  sqrt(mean((obs - pred)^2, na.rm = TRUE))
+}
 
+mae_fun <- function(obs, pred) {
+  mean(abs(obs - pred), na.rm = TRUE)
+}
+
+r2_fun <- function(obs, pred) {
+  ss_res <- sum((obs - pred)^2, na.rm = TRUE)
+  ss_tot <- sum((obs - mean(obs, na.rm = TRUE))^2, na.rm = TRUE)
+  1 - ss_res / ss_tot
+}
+
+skill_score_fun <- function(model_error, baseline_error) {
+  1 - (model_error / baseline_error)
+}
+
+pi_coverage_fun <- function(obs, lower, upper) {
+  mean(obs >= lower & obs <= upper, na.rm = TRUE)
+}
